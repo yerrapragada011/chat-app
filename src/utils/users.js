76ce2626aa87373dfecea1285d15c44b03a756f8
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-17 11:09:47 -0400

    Storing Users: Part 2

--- src/utils/users.js
+++ src/utils/users.js
@@ -40,15 +40,18 @@ const removeUser = (id) => {
   }
 };
 
-addUser({
-  id: 22,
-  username: 'Agasthya  ',
-  room: '  Orlando',
-});
+const getUser = (id) => {
+  return users.find((user) => user.id === id);
+};
 
-console.log(users);
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase();
+  return users.filter((user) => user.room === room);
+};
 
-const removedUser = removeUser(22);
-
-console.log(removeUser);
-console.log(users);
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
