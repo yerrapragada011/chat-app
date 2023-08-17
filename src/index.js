688a4c8615a52c4b3ea3950e9200f7064321cab1
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-08-17 13:25:05 -0400

    Rendering User List

--- src/index.js
+++ src/index.js
@@ -42,6 +42,10 @@ io.on('connection', (socket) => {
         'message',
         generateMessage('Admin', `${user.username} has joined!`)
       );
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
     callback();
   });
@@ -76,7 +80,11 @@ io.on('connection', (socket) => {
     if (user) {
       io.to(user.room).emit(
         'message',
-        generateMessage('Admin', `${user.username} has left!`)
+        generateMessage('Admin', `${user.username} has left!`),
+        io.to(user.room).emit('roomData', {
+          room: user.room,
+          users: getUsersInRoom(user.room),
+        })
       );
     }
   });
